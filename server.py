d9e5a12b4b5f34ab02487db36e9ad3bfbb7740a3
Muhammed Eren <147327203+merenaydin23@users.noreply.github.com>
2026-08-14 09:48:06 +0300

    chore: bind server to dynamic PORT environment variable

--- server.py
+++ server.py
@@ -361,4 +361,6 @@ if os.path.exists(frontend_path):
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=8000)
+    import os
+    port = int(os.getenv("PORT", 8000))
+    uvicorn.run(app, host="0.0.0.0", port=port)
